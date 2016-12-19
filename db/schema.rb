0992fe6617c097237a06085d37ff753d3ab92aae
hukui <tckuikui1234>
2016-12-19 17:31:16 +0800

    fix description string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219043722) do
+ActiveRecord::Schema.define(version: 20161219083711) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161219043722) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.string   "text"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
